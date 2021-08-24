cbddc0db88f5978a9a8e5c79d461ef8a4f8b1643
lightbulb12294 <pierre5340@gmail.com>
2021-08-24 14:17:03 +0800

    [Misc] remove html tag filter whitelist

--- utils/models.py
+++ utils/models.py
@@ -6,5 +6,6 @@ from utils.xss_filter import XSSHtml
 
 class RichTextField(models.TextField):
     def get_prep_value(self, value):
-        with XSSHtml() as parser:
-            return parser.clean(value or "")
+    #     with XSSHtml() as parser:
+    #         return parser.clean(value or "")
+        return value or ""
