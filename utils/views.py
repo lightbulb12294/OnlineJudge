bd76ca27d7e74866d21444da53a4b012c3e71b81
lightbulb12294 <pierre5340@gmail.com>
2021-12-19 14:21:29 +0800

    [File] separate files into folders

--- utils/views.py
+++ utils/views.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from django.conf import settings
 from account.serializers import ImageUploadForm, FileUploadForm
 from utils.shortcuts import rand_str
@@ -27,9 +28,10 @@ class SimditorImageUploadAPIView(CSRFExemptAPIView):
                 "success": False,
                 "msg": "Unsupported file format",
                 "file_path": ""})
-        img_name = rand_str(10) + suffix
+        img_dir = rand_str(10)
+        Path(os.path.join(settings.UPLOAD_DIR, img_dir)).mkdir(parents=True, exist_ok=True)
         try:
-            with open(os.path.join(settings.UPLOAD_DIR, img_name), "wb") as imgFile:
+            with open(os.path.join(settings.UPLOAD_DIR, img_dir, img.name), "wb") as imgFile:
                 for chunk in img:
                     imgFile.write(chunk)
         except IOError as e:
@@ -41,7 +43,7 @@ class SimditorImageUploadAPIView(CSRFExemptAPIView):
         return self.response({
             "success": True,
             "msg": "Success",
-            "file_path": f"{settings.UPLOAD_PREFIX}/{img_name}"})
+            "file_path": f"{settings.UPLOAD_PREFIX}/{img_dir}/{img.name}"})
 
 
 class SimditorFileUploadAPIView(CSRFExemptAPIView):
@@ -58,9 +60,10 @@ class SimditorFileUploadAPIView(CSRFExemptAPIView):
             })
 
         suffix = os.path.splitext(file.name)[-1].lower()
-        file_name = rand_str(10) + suffix
+        file_dir = rand_str(10)
+        Path(os.path.join(settings.UPLOAD_DIR, file_dir)).mkdir(parents=True, exist_ok=True)
         try:
-            with open(os.path.join(settings.UPLOAD_DIR, file_name), "wb") as f:
+            with open(os.path.join(settings.UPLOAD_DIR, file_dir, file.name), "wb") as f:
                 for chunk in file:
                     f.write(chunk)
         except IOError as e:
@@ -71,5 +74,5 @@ class SimditorFileUploadAPIView(CSRFExemptAPIView):
         return self.response({
             "success": True,
             "msg": "Success",
-            "file_path": f"{settings.UPLOAD_PREFIX}/{file_name}",
+            "file_path": f"{settings.UPLOAD_PREFIX}/{file_dir}/{file.name}",
             "file_name": file.name})
